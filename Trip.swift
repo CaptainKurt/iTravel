0f2a79b19e2356a091a3f2677dc6e8622b4d78aa
CaptainKurt <walkkg12@wfu.edu>
2014-10-20 12:54:39 +0200

    Implemented persistence data storage

--- Trip.swift
+++ Trip.swift
@@ -24,4 +24,37 @@ class Trip {
         self.location = location
         self.image = image
     }
+    
+    convenience init(dictionary: NSDictionary) {
+        
+        var title = dictionary["title"] as? String
+        var description = dictionary["description"] as? String
+        var location = dictionary["location"] as? String
+        
+        assert(title != nil && description != nil && location != nil, "the dictionary must have title, description and location keys")
+        
+        var imageData = dictionary["image-data"] as? NSData
+        var image: UIImage?
+        if imageData != nil {
+            image = UIImage(data: imageData!)
+        }
+        
+        self.init(title: title!, location: location!, description: description!, image: image)
+    }
+    
+    func toPropertyListObject() -> NSDictionary
+    {
+        
+        var dictionary: NSMutableDictionary = [
+            "title": title,
+            "description": description,
+            "location": location
+        ]
+        
+        if image != nil {
+            dictionary["image-data"] = UIImageJPEGRepresentation(image, 0.7)
+        }
+        
+        return dictionary
+    }
 }
--- TripManager.swift
+++ TripManager.swift
@@ -19,34 +19,35 @@ class TripManager {
         return tripManagerInstance
     }
     
+    
     init() {
-        let icelandTrip = Trip(
-            title: "Fire and Ice",
-            location: "Iceland",
-            description: "Iceland is a Nordic island country marking the juncture between the North Atlantic and the Arctic Ocean. Iceland is volcanically and geologically active. Icelandic culture is founded upon the nation's Norse heritage. Most Icelanders are descendants of Norse and Gaelic settlers.",
-            image: UIImage(named: "iceland"))
-        trips.append(icelandTrip)
-        
-        let berlinTrip = Trip(
-            title: "City of the Bear",
-            location: "Berlin",
-            description: "Berlin is the capital city of Germany and one of the 16 states of Germany. Berlin is a world city of culture, politics, media, and science. Its urban setting and historical legacy have made it a popular location for international film productions. A bear appears in the coat of arms of the city.",
-            image: UIImage(named: "berlin"))
-        trips.append(berlinTrip)
-        
-        let icelandTrip2 = Trip(
-            title: "Fire and Ice2",
-            location: "Iceland",
-            description: "Iceland is a Nordic island country marking the juncture between the North Atlantic and the Arctic Ocean. Iceland is volcanically and geologically active. Icelandic culture is founded upon the nation's Norse heritage. Most Icelanders are descendants of Norse and Gaelic settlers.",
-            image: UIImage(named: "iceland"))
-        trips.append(icelandTrip2)
-        
-        let berlinTrip2 = Trip(
-            title: "City of the Bear2",
-            location: "Berlin",
-            description: "Berlin is the capital city of Germany and one of the 16 states of Germany. Berlin is a world city of culture, politics, media, and science. Its urban setting and historical legacy have made it a popular location for international film productions. A bear appears in the coat of arms of the city.",
-            image: UIImage(named: "berlin"))
-        trips.append(berlinTrip2)
+//        let icelandTrip = Trip(
+//            title: "Fire and Ice",
+//            location: "Iceland",
+//            description: "Iceland is a Nordic island country marking the juncture between the North Atlantic and the Arctic Ocean. Iceland is volcanically and geologically active. Icelandic culture is founded upon the nation's Norse heritage. Most Icelanders are descendants of Norse and Gaelic settlers.",
+//            image: UIImage(named: "iceland"))
+//        trips.append(icelandTrip)
+//        
+//        let berlinTrip = Trip(
+//            title: "City of the Bear",
+//            location: "Berlin",
+//            description: "Berlin is the capital city of Germany and one of the 16 states of Germany. Berlin is a world city of culture, politics, media, and science. Its urban setting and historical legacy have made it a popular location for international film productions. A bear appears in the coat of arms of the city.",
+//            image: UIImage(named: "berlin"))
+//        trips.append(berlinTrip)
+//        
+//        let icelandTrip2 = Trip(
+//            title: "Fire and Ice2",
+//            location: "Iceland",
+//            description: "Iceland is a Nordic island country marking the juncture between the North Atlantic and the Arctic Ocean. Iceland is volcanically and geologically active. Icelandic culture is founded upon the nation's Norse heritage. Most Icelanders are descendants of Norse and Gaelic settlers.",
+//            image: UIImage(named: "iceland"))
+//        trips.append(icelandTrip2)
+//        
+//        let berlinTrip2 = Trip(
+//            title: "City of the Bear2",
+//            location: "Berlin",
+//            description: "Berlin is the capital city of Germany and one of the 16 states of Germany. Berlin is a world city of culture, politics, media, and science. Its urban setting and historical legacy have made it a popular location for international film productions. A bear appears in the coat of arms of the city.",
+//            image: UIImage(named: "berlin"))
+//        trips.append(berlinTrip2)
     }
     
     func filteredTripsForSearchText(searchText: String) -> [Trip] {
@@ -59,6 +60,40 @@ class TripManager {
         }
         
         return filteredTrips
+    }
+    
+    func documentsDirectoryPath() -> String {
+        
+        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as [String]
+        
+        return paths.first!
+    }
+    
+    func saveTrips() {
+        let docPath = documentsDirectoryPath()
+        let tripsFilePath = docPath.stringByAppendingPathComponent("trips.plist")
+        
+        let tripPropertyListObjects = NSMutableArray()
+        for trip in trips {
+            tripPropertyListObjects.addObject(trip.toPropertyListObject())
+        }
+        
+        let data = NSPropertyListSerialization.dataWithPropertyList(tripPropertyListObjects, format: .XMLFormat_v1_0, options: 0, error: nil)
+        data?.writeToFile(tripsFilePath, atomically: true)
+    }
+    
+    func loadTrips() {
+        let tripsFilePath = documentsDirectoryPath().stringByAppendingPathComponent("trips.plist")
+        
+        let plistData: NSData? = NSData(contentsOfFile: tripsFilePath)
+        if plistData != nil {
+            let tripPropertyListObjects =  NSPropertyListSerialization.propertyListWithData(plistData!, options: 0, format: nil, error: nil) as NSArray
+            
+            for tripDictionary in tripPropertyListObjects {
+                let trip = Trip(dictionary: tripDictionary as NSDictionary)
+                trips.append(trip)
+            }
+        }
     }
     
 }
